e9a18bbab75378deecce3f9dccdfed91475ecb47
kizzeldin <kabier.izzeldin@mail.utoronto.ca>
2024-10-20 18:50:47 -0400

    commit

---
+++ R/toydata.R
@@ -0,0 +1,15 @@
+#' Data
+#'
+#' Data from the course website
+#'
+#' @docType data
+#' @format A data frame with 100 rows and 3 variables:
+#' \describe{
+#'   \item {y}{a binary outcome}
+#'   \item{x1}{a continuous predictor}
+#'   \item{x2}{a binary predictor}
+#' }
+#' @source CHL8010 Course Website
+#' @references Aya Mitani
+
+devtools::document()
